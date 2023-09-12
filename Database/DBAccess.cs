658ea5479583598cf797f2f4480a6c9614f1c416
Holger Jüptner <holger.jueptner@creativity-gmbh.de>
2023-09-12 10:34:37 +0200

    changes

---
+++ Database/DBAccess.cs
@@ -0,0 +1,45 @@
+﻿using Restaurant.Models;
+
+namespace Restaurant.Database
+{
+    public static class DBAccess
+    {
+        public static Bestellung GetOrder()
+        {
+            // TODO
+            return new Bestellung();
+        }
+
+        public static void InsertOrder()
+        {
+            // TODO
+        }
+
+        public static void UpdateOrder()
+        {
+            // TODO
+        }
+
+        public static Artikel GetArticle()
+        {
+            // TODO
+            return new Artikel();
+        }
+
+        public static Kellner GetKellner()
+        {
+            return new Kellner();
+        }
+
+        public static Tisch GetTisch()
+        {
+            // TODO
+        }
+
+        public static Bestellpositionen GetBestellpositionen()
+        {
+            // TODO
+            return new Bestellpositionen();
+        }
+    }
+}
---
+++ Models/Bestellung.cs
@@ -0,0 +1,10 @@
+﻿namespace Restaurant.Models
+{
+    public class Bestellung
+    {
+        public int ID_Bestellung { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int TableNumber { get; set; }
+        public List<Artikel> OrderList { get; set; }
+    }
+}
