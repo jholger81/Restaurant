93e1958fad1368728e2494af1529b5152355c9c4
jholger81 <116508230+jholger81@users.noreply.github.com>
2023-09-25 14:44:53 +0200

    Add files via upload

---
+++ Models/Rechnung.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant.Models
+{
+    public class Rechnung
+    {
+        public int ID_Bestellung { get; set; }
+        int Trinkgeld { get; set; }
+    }
+}
---
+++ Models/Rechnungposition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant.Models
+{
+    public class Rechnungposition
+    {
+        public int ID_Rechnungposition { get; set; }
+        public int ID_Artikel { get; set; }
+        public int ID_Rechnung { get; set; }
+        public Artikel Artikel { get; set; }
+    }
+}
