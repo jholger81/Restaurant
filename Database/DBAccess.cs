849e3fbaf47f5ba506ff70a7ec210b9472509bc2
jholger81 <116508230+jholger81@users.noreply.github.com>
2023-09-25 14:27:39 +0200

    Add files via upload

--- Database/DBAccess.cs
+++ Database/DBAccess.cs
@@ -12,7 +12,7 @@ namespace Restaurant.Database
             Bestellung bestellung = new Bestellung();
 
             string strTemp = "Data Source=Database.db3";
-            string sql = $"SELECT ID_Bestellung, Datum, Gäste, ID_Tisch, Einnahmen FROM Bestellung WHERE ID_Tisch = {id_Tisch}";
+            string sql = $"SELECT ID_Bestellung, Datum, ID_Tisch FROM Bestellung WHERE ID_Tisch = {id_Tisch}";
             SQLiteConnection sqliteconnection = new SQLiteConnection(strTemp);
             sqliteconnection.Open();
             SQLiteCommand sqlitecommand = new SQLiteCommand(sql, sqliteconnection);
@@ -22,9 +22,7 @@ namespace Restaurant.Database
             {
                 bestellung.ID_Bestellung = sqlitereader.GetInt32(0);
                 bestellung.Datum = DateTime.Parse(sqlitereader.GetString(1));
-                bestellung.Gäste = sqlitereader.GetInt32(2);
-                bestellung.ID_Tisch = sqlitereader.GetInt32(3);
-                bestellung.Einnahmen = sqlitereader.GetInt32(4);
+                bestellung.ID_Tisch = sqlitereader.GetInt32(2);
             }
             sqliteconnection.Close();
 
@@ -87,7 +85,7 @@ namespace Restaurant.Database
             List<Bestellposition> positionen = new List<Bestellposition>();
 
             string strTemp = "Data Source=Database.db3";
-            string sql = $"SELECT ID_Bestellposition, ID_Artikel, ID_Bestellung, bezahlt, geliefert FROM Bestellposition WHERE ID_Bestellung = {id_Bestellung}";
+            string sql = $"SELECT ID_Bestellposition, ID_Artikel, ID_Bestellung, Extras, Geliefert FROM Bestellposition WHERE ID_Bestellung = {id_Bestellung}";
             SQLiteConnection sqliteconnection = new SQLiteConnection(strTemp);
             sqliteconnection.Open();
             SQLiteCommand sqlitecommand = new SQLiteCommand(sql, sqliteconnection);
@@ -99,7 +97,16 @@ namespace Restaurant.Database
                 position.ID_Bestellposition = sqlitereader.GetInt32(0);
                 position.ID_Artikel = sqlitereader.GetInt32(1);
                 position.ID_Bestellung = sqlitereader.GetInt32(2);
-                position.Bezahlt = sqlitereader.GetInt32(3);
+                try
+                {
+                    position.Extras = sqlitereader.GetString(3);
+                }
+                catch (Exception ex)
+                {
+                    position.Extras = null;
+                    Console.WriteLine($"{ex.Message}");
+                }
+                
                 position.Geliefert = sqlitereader.GetInt32(4);
                 positionen.Add(position);
             }
