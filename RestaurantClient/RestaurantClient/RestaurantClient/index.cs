fac4b2caaa900ffbdfd1a928a967d7e3b70d2327
GartenMensch <34944214+GartenMensch@users.noreply.github.com>
2023-10-16 10:53:22 +0200

    Ganz wichtiges Zeug
    Client GUI

---
+++ RestaurantClient/RestaurantClient/RestaurantClient/index.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestaurantClient
+{
+    public partial class index : Form
+    {
+        public index()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form addOrder = new addOrder();
+
+            // Show the settings form
+            addOrder.Show();
+        }
+    }
+}
